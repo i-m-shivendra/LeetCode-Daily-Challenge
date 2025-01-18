2723a0a11f60a86b086bd1325fc31c640f2ad2f9
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-01-19 00:49:19 +0530

    Time: 23 ms (63.43%), Space: 26.2 MB (38.12%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,51 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int dx[4] = {0, 0, 1, -1};
+const int dy[4] = {1, -1, 0, 0};
 class Solution {
-    private int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1,0}};
-
-    public int minCost(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0])); // min-heap
-        int[][] result = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                result[i][j] = Integer.MAX_VALUE;
-            }
-        }
-
-        pq.offer(new int[]{0, 0, 0}); // {cost, i, j}
-        result[0][0] = 0;
-
-        // T.C : O(m*n * log(m*n))
-        // S.C : O(m*n)
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll(); // log(size of pq)
-
-            int currCost = curr[0];
-            int i = curr[1];
-            int j = curr[2];
-
-            if (result[i][j] < currCost) {
-                continue;
-            }
-
-            for (int dir_i = 0; dir_i <= 3; dir_i++) {
-                int i_ = i + dir[dir_i][0];
-                int j_ = j + dir[dir_i][1];
-
-                if (i_ >= 0 && j_ >= 0 && i_ < m && j_ < n) {
-                    int gridDir = grid[i][j];
-                    int dirCost = ((gridDir - 1 != dir_i) ? 1 : 0);
-
-                    int newCost = currCost + dirCost;
-
-                    if (newCost < result[i_][j_]) {
-                        result[i_][j_] = newCost;
-                        pq.offer(new int[]{newCost, i_, j_});
+public:
+    int minCost(vector<vector<int>>& grid) {
+        int r = grid.size(), c = grid[0].size();
+        vector<vector<int>> dist(r, vector<int>(c, INT_MAX));
+        deque<pair<int, int>> dq;
+        dq.emplace_front(0, 0);
+        dist[0][0] = 0;
+        
+        while (!dq.empty()) {
+            auto [x, y] = dq.front(); dq.pop_front();
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dx[i], ny = y + dy[i];
+                if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
+                    int cost = (i + 1 == grid[x][y]) ? 0 : 1;
+                    if (dist[x][y] + cost < dist[nx][ny]) {
+                        dist[nx][ny] = dist[x][y] + cost;
+                        if (cost == 0) {
+                            dq.emplace_front(nx, ny);
+                        } else {
+                            dq.emplace_back(nx, ny);
+                        }
                     }
                 }
             }
         }
-        return result[m - 1][n - 1]; // min cost to reach (m-1, n-1)
+        return dist[r-1][c-1];
     }
-}
+};
