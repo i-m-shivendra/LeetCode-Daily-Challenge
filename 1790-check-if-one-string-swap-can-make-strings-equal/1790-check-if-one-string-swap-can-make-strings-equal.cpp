0a217317c04b6fcb9220dfc062aa3c4c36a3320f
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-02-05 23:19:02 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.33%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n = s1.size();
+
+        if(s1==s2)
+        {
+            return true;
+        }
+
+        int diffCount = 0;
+
+        int freq1[26] = {0};
+        int freq2[26] = {0};
+
+        for(int i=0;i<n;i++)
+        {
+            if(s1[i] != s2[i])
+            {
+                diffCount++;
+            }
+
+            if(diffCount > 2)
+            {
+                return false;
+            }
+
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+
+        }
+
+        for(int i=0;i<26;i++)
+        {
+            if(freq1[i] != freq2[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
