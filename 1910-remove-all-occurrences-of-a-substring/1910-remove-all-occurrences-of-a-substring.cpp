1dcf6b7d404989cf3e83002a0e1c47475b5c2796
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-02-12 00:24:30 +0530

    Time: 9 ms (5.85%), Space: 25.4 MB (5.32%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,23 +1,72 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        //jab tak string me characters hain
-        while(true)
+
+    bool check(stack <char>& st, string& part, int n)
         {
-            //us particular index se wo substring find karo
-            int index = s.find(part);
+            stack <char> tempStack = st;
 
-            //agr substring nhi mila to
-            if(index == string::npos)
+            for(int idx = n-1; idx >= 0; idx--)
             {
-                break;
+                if(tempStack.top() != part[idx])
+                {
+                    return false;
+                }
+                tempStack.pop();
             }
-
-           //agr mil jata hai to us particular index se
-           //lkr part ke size tak ke charcters ko hata do
-            s.erase(index,part.size());
+            return true;
         }
-        //last me wo hi string return kar do
-        return s;
+
+    string removeOccurrences(string s, string part) {
+
+        // //Approach-1 Brute-force
+        // //jab tak string me characters hain
+        // while(true)
+        // {
+        //     //us particular index se wo substring find karo
+        //     int index = s.find(part);
+
+        //     //agr substring nhi mila to
+        //     if(index == string::npos)
+        //     {
+        //         break;
+        //     }
+
+        //    //agr mil jata hai to us particular index se
+        //    //lkr part ke size tak ke charcters ko hata do
+        //     s.erase(index,part.size());
+        // }
+        // //last me wo hi string return kar do
+        // return s;
+
+
+        
+       
+        //Approach-2 Using Stack
+
+        stack <char> st;
+
+        int m = s.size();
+        int n = part.size();
+
+        for(int i = 0; i < m ;i++)
+        {
+            st.push(s[i]);
+
+            if(st.size() >= n && check(st,part,n) == true)
+            {
+                for(int j = 0; j<n;j++)
+                {
+                    st.pop();
+                }
+            }
+        }
+        string ans = "";
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
