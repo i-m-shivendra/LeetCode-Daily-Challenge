e103e164a2942fafe6b46206c5851454e837c7fb
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-02-12 00:29:20 +0530

    Time: 1 ms (50.91%), Space: 12.2 MB (8.29%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,72 +1,18 @@
 class Solution {
 public:
-
-    bool check(stack <char>& st, string& part, int n)
-        {
-            stack <char> tempStack = st;
-
-            for(int idx = n-1; idx >= 0; idx--)
-            {
-                if(tempStack.top() != part[idx])
-                {
-                    return false;
-                }
-                tempStack.pop();
-            }
-            return true;
-        }
-
     string removeOccurrences(string s, string part) {
-
-        // //Approach-1 Brute-force
-        // //jab tak string me characters hain
-        // while(true)
-        // {
-        //     //us particular index se wo substring find karo
-        //     int index = s.find(part);
-
-        //     //agr substring nhi mila to
-        //     if(index == string::npos)
-        //     {
-        //         break;
-        //     }
-
-        //    //agr mil jata hai to us particular index se
-        //    //lkr part ke size tak ke charcters ko hata do
-        //     s.erase(index,part.size());
-        // }
-        // //last me wo hi string return kar do
-        // return s;
-
-
-        
-       
-        //Approach-2 Using Stack
-
-        stack <char> st;
-
-        int m = s.size();
+        string ans = "";
         int n = part.size();
 
-        for(int i = 0; i < m ;i++)
+        for(char &ch:s)
         {
-            st.push(s[i]);
+            ans.push_back(ch);
 
-            if(st.size() >= n && check(st,part,n) == true)
+            if(ans.size() >= n && ans.substr(ans.size() - n) == part)
             {
-                for(int j = 0; j<n;j++)
-                {
-                    st.pop();
-                }
+                ans.erase(ans.size() - n);
             }
         }
-        string ans = "";
-        while(!st.empty())
-        {
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
