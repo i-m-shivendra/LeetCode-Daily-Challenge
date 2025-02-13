7c8dc2ee222623a9ba52cdf01e24b54ac0ab75d2
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-02-13 23:50:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        //Heapify - O(log n)
+        priority_queue <long,vector<long>,greater<long>> pq(begin(nums),end(nums));
+
+        int count = 0;
+
+        while(!pq.empty() && pq.top() >= k)
+        {
+            long smallest = pq.top();   //minimum element ko nikal lo
+            pq.pop();
+
+            long secondSmallest = pq.top(); //second minimum element ko nikal lo
+            pq.pop();
+
+            pq.push(smallest*2 + secondSmallest);
+
+            count++;
+        }
+        return count;;
+    }
+};
