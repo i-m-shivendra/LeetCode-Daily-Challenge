b59f5613024df6869189b3c4c2f7222ff05f6cd4
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-03-02 00:51:16 +0530

    Time: 0 ms (100%), Space: 13.2 MB (62.06%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
         int j = 0;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             if(i+1 < n && nums[i] == nums[i+1] && nums[i] != 0){
                 nums[i] = nums[i] * 2;
                 nums[i+1] = 0;
