507b7c39ff5e1f951e28e62580c74095d769dae9
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-03-19 21:53:00 +0530

    Time: 10 ms (37.03%), Space: 139.9 MB (74.5%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -9,8 +9,8 @@ public:
                 nums[i] = 1 - nums[i];
                 nums[i+1] = 1 - nums[i+1];
                 nums[i+2] = 1 - nums[i+2];
+                ans += 1;
             }
-            ans += 1;
         }
 
         for(int i=0;i<n;i++){
