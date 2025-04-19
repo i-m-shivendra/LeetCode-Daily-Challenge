602d10c2d2b97958e0d8a2505d85abdb01f6ff3c
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-04-19 10:40:15 +0530

    Time: 22 ms (99.09%), Space: 60.5 MB (38.74%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        int n = nums.size();
-        int ans = 0;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                int sum = nums[i] + nums[j];
-                if(sum >= lower && sum <= upper){
-                    ans++;
-                }
-            }
+    long long countAtMost(vector<int>& nums, long long comp){
+        long long ans = 0;
+        for (int i = 0, j = nums.size() - 1; i < j; i++) {
+            while(i < j && nums[i] + nums[j] > comp) --j;
+            ans += j - i;
         }
         return ans;
+    }
+
+    long long countFairPairs(vector<int>& nums, int lower, int upper){
+        sort(nums.begin(), nums.end());
+        return countAtMost(nums, upper) - countAtMost(nums, lower - 1);
     }
 };
