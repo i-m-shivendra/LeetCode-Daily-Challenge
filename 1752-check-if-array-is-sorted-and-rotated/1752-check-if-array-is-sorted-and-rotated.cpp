231069c3007a71751490230b4b2d1b59ff57f9af
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-05-31 15:31:02 +0530

    Time: 0 ms (100%), Space: 11.2 MB (45.55%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,10 +4,10 @@ public:
         int n = nums.size();
         int count = 0;
         for(int i=0;i<n;i++){
-            if( nums[i] > nums[(i+1) % n] ){
+            if(nums[i] > nums[(i+1) % n]){
                 count += 1;
             }
-        } 
-        return count<=1;
+        }
+        return count <= 1;
     }
 };
