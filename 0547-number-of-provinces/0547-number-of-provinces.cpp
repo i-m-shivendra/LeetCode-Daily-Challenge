c0333c6e38c122f2b219a504c544906b23213745
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-06-17 21:39:23 +0530

    Time: 0 ms (100%), Space: 21.1 MB (17.86%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,24 +1,20 @@
 class Solution {
 private:
-   void dfs(int node, vector<vector<int>>& adjList,
-   vector<int>& visited){
+  void dfs(int node, vector<vector<int>>& adjList,
+  vector<int>& visited){
     visited[node] = 1;
-
     for(auto it : adjList[node]){
         if(!visited[it]){
             dfs(it, adjList, visited);
         }
     }
-    
-   }
+  }
+
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int V  = isConnected.size();
-
-        //Defining an adjacency list
+        int V = isConnected.size();
         vector<vector<int>> adjList(V);
 
-        //Converting adjacency matrix to List
         for(int i=0;i<V;i++){
             for(int j=0;j<V;j++){
                 if(isConnected[i][j] == 1 && i != j){
@@ -28,19 +24,15 @@ public:
             }
         }
 
-        //Declaring an visited array
         vector<int> visited(V, 0);
-
-        //Declaring count variable
         int count = 0;
 
         for(int i=0;i<V;i++){
             if(!visited[i]){
-                dfs(i, adjList, visited);
                 count += 1;
+                dfs(i, adjList, visited);
             }
         }
-
         return count;
     }
 };
